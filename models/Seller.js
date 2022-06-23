aecbdfc5f5095a8e598fb0f5c5673e2ede0c3f2f
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-06-23 19:05:13 +0530

    seller schema added

---
+++ models/Seller.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+
+const sellerSchema = new Schema(
+    {
+        products: {
+            type: [String],
+            default: "null"
+        },
+        mySellings: {
+            type: [String],
+            default: "null"
+        },
+        address: {
+            type: String,
+            validate: [
+                {   
+                    validator: function(value){
+                        return !value.includes("$")
+                    },
+                    message: "Incorrect character!"
+                },
+                {   
+                    validator: function(value){
+                        return !value.includes("<")
+                    },
+                    message: "Incorrect character!"
+                },
+                {   
+                    validator: function(value){
+                        return !value.includes(">")
+                    },
+                    message: "Incorrect character!"
+                },
+                {   
+                    validator: function(value){
+                        return !value.includes("?")
+                    },
+                    message: "Incorrect character!"
+                }
+            ],
+            required: true
+        }
+    }
+    
+)
+
+export default mongoose.model('Seller', sellerSchema)
