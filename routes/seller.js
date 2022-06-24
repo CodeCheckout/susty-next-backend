02b7511fefb4169dee0b4fc7cc02f404b5619b1e
SubodaDabarera <SubodaDabarera@users.noreply.github.com>
2022-06-24 05:30:12 +0000

    Prettified Code!

--- routes/seller.js
+++ routes/seller.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import {getSellerProducts} from '../controllers/seller'
 
-const router = express.Router();
+const router = express.Router()
 router.get('/seller/products', getSellerProducts)
 
 module.exports = router
