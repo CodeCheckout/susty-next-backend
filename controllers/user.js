d3e52e596d99037b3b9d85d3b59a173fde886776
gmnchamikara <gmnchamikara@users.noreply.github.com>
2022-07-06 01:41:26 +0000

    Prettified Code!

--- controllers/user.js
+++ controllers/user.js
@@ -1,120 +1,104 @@
-import User from "../models/user";
+import User from '../models/user'
 
 //add user
 export const adduser = async (req, res) => {
-  const {
-    name,
-    image,
-    role,
-    userId,
-    email,
-    address
+    const {name, image, role, userId, email, address} = req.body
 
-} = req.body
-
-const newUser = new User({
-  name,
-  image,
-  role,
-  userId,
-  email,
-  address
-})
-
-await User.create(newUser)
-    .then((user) => {
-        return res.status(200).json({
-            success: true,
-            message: 'User added successfully!',
-            user,
-        })
+    const newUser = new User({
+        name,
+        image,
+        role,
+        userId,
+        email,
+        address,
     })
-    .catch((error) => {
-        return res.status(500).json({
-            success: false,
-            message: 'Failed to add User!',
-            error,
+
+    await User.create(newUser)
+        .then((user) => {
+            return res.status(200).json({
+                success: true,
+                message: 'User added successfully!',
+                user,
+            })
         })
-    })
+        .catch((error) => {
+            return res.status(500).json({
+                success: false,
+                message: 'Failed to add User!',
+                error,
+            })
+        })
 }
 
 //add address
 
-
 //update user name
 export const updateUserName = async (req, res) => {
-  const { address } = req.body;
+    const {address} = req.body
 
-  const newAddress = new User({
-    address,
-  });
-
-  await User.updateOne(newAddress)
-    .then((address) => {
-      return res.status(200).json({
-        success: true,
-        message: "Username updated successfully!",
+    const newAddress = new User({
         address,
-      });
     })
-    .catch((error) => {
-      return res.status(500).json({
-        success: false,
-        message: "Failed to update Username!",
-        error,
-      });
-    });
-};
+
+    await User.updateOne(newAddress)
+        .then((address) => {
+            return res.status(200).json({
+                success: true,
+                message: 'Username updated successfully!',
+                address,
+            })
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                success: false,
+                message: 'Failed to update Username!',
+                error,
+            })
+        })
+}
 
 //update user roles
 export const updateUserRole = async (req, res) => {
-  
-const {id} = req.params.id;
-const upadate = req.body;
-const options = {new: true};
+    const {id} = req.params.id
+    const upadate = req.body
+    const options = {new: true}
 
-await User.findByIdAndUpdate(id, upadate, options )
-  .then((address)=>{
-    return res.status(200).json({
-      success: true,
-      message: "Userrole updated successfully!",
-      address,
-    });
-  })
-    .catch((error) => {
-      return res.status(500).json({
-        success: false,
-        message: "Failed to update UserRole!",
-        error,
-      });
-    });
-};
+    await User.findByIdAndUpdate(id, upadate, options)
+        .then((address) => {
+            return res.status(200).json({
+                success: true,
+                message: 'Userrole updated successfully!',
+                address,
+            })
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                success: false,
+                message: 'Failed to update UserRole!',
+                error,
+            })
+        })
+}
 
 //update user address
-export const updateUserAddress = async (req, res) => {
-
-};
-
+export const updateUserAddress = async (req, res) => {}
 
 //get user address
 export const getUserAddress = async (req, res) => {
-  const { id } = req.query;
+    const {id} = req.query
 
-  await User.findById(id)
-    .then((user) => {
-      console.log(user.email)
-      return res.status(200).json({
-        success: true,
-        message: "Address fetched successfully!",
-        
-      });
-    })
-    .catch((error) => {
-      return res.status(400).json({
-        success: false,
-        message: "Failed to fetch Address!",
-      });
-    });
-};
-
-
+    await User.findById(id)
+        .then((user) => {
+            console.log(user.email)
+            return res.status(200).json({
+                success: true,
+                message: 'Address fetched successfully!',
+            })
+        })
+        .catch((error) => {
+            return res.status(400).json({
+                success: false,
+                message: 'Failed to fetch Address!',
+            })
+        })
+}
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,11 @@
 import express from 'express'
-import {adduser, updateUserName, updateUserRole, updateUserAddress, getUserAddress} from '../controllers/user'
+import {
+    adduser,
+    updateUserName,
+    updateUserRole,
+    updateUserAddress,
+    getUserAddress,
+} from '../controllers/user'
 
 const router = express.Router()
 
