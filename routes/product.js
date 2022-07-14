f1de219ceca11e5376c5d4d7e82b8adcbe493ca4
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-07-14 23:16:26 +0530

    settle the searching product names

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ import {
     fetchProducts,
     filterProducts,
     getSingleProduct,
+    searchProducts
 } from '../controllers/product'
 
 const router = express.Router()
@@ -12,5 +13,6 @@ router.post('/product/add', addProduct)
 router.get('/product/fetch-products', fetchProducts)
 router.get('/product/fetch-single-product', getSingleProduct)
 router.get('/product/filter-products', filterProducts)
+router.get('/product/search-products', searchProducts)
 
 module.exports = router
