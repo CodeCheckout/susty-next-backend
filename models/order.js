786924772bbfdbddc4a165568c1d9186e7a051b9
SubodaDabarera <SubodaDabarera@users.noreply.github.com>
2022-07-15 11:28:54 +0000

    Prettified Code!

--- models/order.js
+++ models/order.js
@@ -1,24 +1,24 @@
-import mongoose from "mongoose";
-import express from "express";
+import mongoose from 'mongoose'
+import express from 'express'
 
-const { Schema } = mongoose;
+const {Schema} = mongoose
 const orderSchema = new Schema({
-  productId: {
-    type: String,
-    required: true,
-  },
-  seller: {
-    type: Schema.Types.ObjectId,
-    required: true,
-  },
-  buyer: {
-    type: Schema.Types.ObjectId,
-    required: true,
-  },
-  PayPalLog: {
-    type: JSON,
-    required: true,
-  },
-});
+    productId: {
+        type: String,
+        required: true,
+    },
+    seller: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    buyer: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    PayPalLog: {
+        type: JSON,
+        required: true,
+    },
+})
 
-export default mongoose.model("Order", orderSchema);
+export default mongoose.model('Order', orderSchema)
--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { getBuyerOrders, getSellerOrders, placeOrder } from '../controllers/order';
+import {getBuyerOrders, getSellerOrders, placeOrder} from '../controllers/order'
 const router = express.Router()
 
-router.get('/orders/fetch-seller-orders', getSellerOrders )
-router.get('/orders/fetch-buyer-orders', getBuyerOrders )
+router.get('/orders/fetch-seller-orders', getSellerOrders)
+router.get('/orders/fetch-buyer-orders', getBuyerOrders)
 
 // for testing
 router.post('/orders/create-order', placeOrder)
 
-module.exports = router;
+module.exports = router
