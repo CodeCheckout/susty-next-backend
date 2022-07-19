a676fa38d61a83a4a2d34c2066882abc7e3cdf7c
SubodaDabarera <SubodaDabarera@users.noreply.github.com>
2022-07-19 17:18:53 +0000

    Prettified Code!

--- controllers/order.js
+++ controllers/order.js
@@ -1,80 +1,80 @@
-import Order from "../models/order";
+import Order from '../models/order'
 
 export const getSellerOrders = async (req, res) => {
-  const { sellerId } = req.query;
+    const {sellerId} = req.query
 
-  await Order.find({ seller: sellerId })
-    .then((orders) => {
-      return res.status(200).json({
-        success: true,
-        message: "Seller orders fetched successfully",
-        orders,
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json({
-        success: false,
-        message: "Failed to fetch seller orders",
-        error,
-      });
-    });
-};
+    await Order.find({seller: sellerId})
+        .then((orders) => {
+            return res.status(200).json({
+                success: true,
+                message: 'Seller orders fetched successfully',
+                orders,
+            })
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                success: false,
+                message: 'Failed to fetch seller orders',
+                error,
+            })
+        })
+}
 
 export const getBuyerOrders = async (req, res) => {
-  const { buyerId } = req.query;
+    const {buyerId} = req.query
 
-  await Order.find({ buyer: buyerId })
-    .then((orders) => {
-      return res.status(200).json({
-        success: true,
-        message: "Buyer orders fetched successfully",
-        orders,
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json({
-        success: false,
-        message: "Failed to fetch buyer orders",
-        error,
-      });
-    });
-};
+    await Order.find({buyer: buyerId})
+        .then((orders) => {
+            return res.status(200).json({
+                success: true,
+                message: 'Buyer orders fetched successfully',
+                orders,
+            })
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                success: false,
+                message: 'Failed to fetch buyer orders',
+                error,
+            })
+        })
+}
 
 // for testing
 export const placeOrder = async (req, res) => {
-  const { productId, sellerId, buyerId, price, PayPalLog } = req.body;
+    const {productId, sellerId, buyerId, price, PayPalLog} = req.body
 
-  const newOrder = new Order({
-    productId,
-    seller: sellerId,
-    buyer: buyerId,
-    price,
-    buyerName,
-    sellerName,
-    PayPalLog,
-  });
-
-  await Order.create(newOrder).then((order) => {
-    return res.status(200).json({
-      success: true,
-      message: "Order placed successfully",
-      order,
-    });
-  });
-
-  await Order.create(newOrder)
-    .then((order) => {
-      return res.status(200).json({
-        success: true,
-        message: "Order placed successfully",
-        order,
-      });
+    const newOrder = new Order({
+        productId,
+        seller: sellerId,
+        buyer: buyerId,
+        price,
+        buyerName,
+        sellerName,
+        PayPalLog,
     })
-    .catch((error) => {
-      return res.status(500).json({
-        success: false,
-        message: "Failed to place order",
-        error,
-      });
-    });
-};
+
+    await Order.create(newOrder).then((order) => {
+        return res.status(200).json({
+            success: true,
+            message: 'Order placed successfully',
+            order,
+        })
+    })
+
+    await Order.create(newOrder)
+        .then((order) => {
+            return res.status(200).json({
+                success: true,
+                message: 'Order placed successfully',
+                order,
+            })
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                success: false,
+                message: 'Failed to place order',
+                error,
+            })
+        })
+}
--- models/order.js
+++ models/order.js
@@ -1,42 +1,42 @@
-import mongoose from "mongoose";
-import express from "express";
+import mongoose from 'mongoose'
+import express from 'express'
 
-const { Schema } = mongoose;
+const {Schema} = mongoose
 const orderSchema = new Schema(
-  {
-    productId: {
-      type: String,
-      required: true,
+    {
+        productId: {
+            type: String,
+            required: true,
+        },
+        productName: {
+            type: String,
+        },
+        seller: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
+        buyer: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
+        buyerName: {
+            type: String,
+        },
+        sellerName: {
+            type: String,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        PayPalLog: {
+            type: JSON,
+            required: true,
+        },
     },
-    productName: {
-      type: String,
-    },
-    seller: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
-    buyer: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
-    buyerName: {
-      type: String,
-    },
-    sellerName: {
-      type: String,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    PayPalLog: {
-      type: JSON,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+        timestamps: true,
+    }
+)
 
-export default mongoose.model("Order", orderSchema);
+export default mongoose.model('Order', orderSchema)
