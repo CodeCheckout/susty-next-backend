c431c558f2f17d4da83e4bee5243820fc416fe52
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-09-01 21:17:54 +0530

    prettified

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {authenticateUser, emailSignIn, forgotPassword, loginUser, registerUser, resetPassword, verifyResetToken} from '../controllers/auth'
+import {authenticateUser, emailSignIn, forgotPassword, registerUser, resetPassword, verifyResetToken} from '../controllers/auth'
 
 const router = express.Router()
 
@@ -8,7 +8,6 @@ router.post('/user/emailSignIn', emailSignIn)
 
 // from JWT part
 router.post('/user/register', registerUser);
-router.post('/user/login', loginUser);
 router.post('/user/forgotPassword', forgotPassword);
 router.put('/user/resetPassword', resetPassword);
 router.post('/user/verifyPasswordToken', verifyResetToken);
